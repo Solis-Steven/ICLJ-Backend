375dc0b3391565444fb0824c55a07b06485ad961
Solis-Steven <kp07761@gmail.com>
2023-12-26 00:19:29 -0600

    dataBaseConnection file has been changed

--- config/dataBaseConnection.js
+++ config/dataBaseConnection.js
@@ -4,9 +4,7 @@ import mongoose from "mongoose";
 const dataBaseConnection = async() => {
     try {
         const uri = process.env.MONGODB_URI || "";
-        console.log("uri", uri)
         const connection = await mongoose.connect(uri);
-
 
         const url = `${connection.connection.host}: ${connection.connection.port}`;
         console.log(`Connected: ${url}`);
