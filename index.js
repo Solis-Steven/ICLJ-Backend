59471c6eff39d85cd0382cd6203b9e63a7987272
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2023-12-27 22:14:23 -0500

    se agrego la ruta
    al idex.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dataBaseConnection from "./config/dataBaseConnection.js";
 import cors from "cors";
 import dotenv from 'dotenv';
-
+import announcementRoutes from './routes/announcementRoutes.js';
 dotenv.config();
 
 const app = new express();
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 dataBaseConnection();
-
+app.use('/api/announcements', announcementRoutes)
 const server = app.listen(8000, () => {
     console.log("Server runs");
 })
