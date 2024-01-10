5b2b755cbb92ba700a52b7af6a6566ca25b554de
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-01-09 18:19:39 -0600

    cambios
    servidor y announcement controller

--- controllers/Announcement.controller.js
+++ controllers/Announcement.controller.js
@@ -1,16 +1,20 @@
-import Announcement from '../models/Announcement.model.js';
+import Announcement from "../models/Announcement.model.js";
 
 export const getAllAnnouncements = async (req, res) => {
- try {
-    const announcements = await Announcement.find();
-    res.status(200).json(announcements);
- } catch (error) {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const announcements = await Announcement.find()
+      .select("-createdAt -updatedAt -__v")
+      .skip((page - 1) * limit)
+      .limit(limit);
+    res.json(announcements);
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const createAnnouncement = async (req, res) => {
- try {
+  try {
     const announcement = new Announcement({
       name: req.body.name,
       description: req.body.description,
@@ -21,52 +25,52 @@ export const createAnnouncement = async (req, res) => {
     await announcement.save();
 
     res.status(201).json(announcement);
- } catch (error) {
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const getAnnouncement = async (req, res) => {
- try {
+  try {
     const announcement = await Announcement.findById(req.params.id);
-    if(!announcement){
-      return res.status(404).json({ message: 'Anuncio no encontrado' });
+    if (!announcement) {
+      return res.status(404).json({ message: "Anuncio no encontrado" });
     }
     res.status(200).json(announcement);
- } catch (error) {
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const updateAnnouncement = async (req, res) => {
-   const AnnouncementToUpdate = await Announcement.findById(req.params.id);
-   if(!AnnouncementToUpdate) {
-     return res.status(404).json({ message: 'Anuncio no encontrado' });
-   }
+  const AnnouncementToUpdate = await Announcement.findById(req.params.id);
+  if (!AnnouncementToUpdate) {
+    return res.status(404).json({ message: "Anuncio no encontrado" });
+  }
 
-   const {name, description, date, image} = req.body
-   AnnouncementToUpdate.name = name || AnnouncementToUpdate.name;
-    AnnouncementToUpdate.description = description || AnnouncementToUpdate.description;
-    AnnouncementToUpdate.date = date || AnnouncementToUpdate.date;
-    AnnouncementToUpdate.image = image || AnnouncementToUpdate.image;
- try {
-   const AnnouncementSaved = await AnnouncementToUpdate.save();
-   res.json(AnnouncementSaved);
-  
- } catch (error) {
-   res.status(500).json({msg: "Internal Server Error"})
- }
+  const { name, description, date, image } = req.body;
+  AnnouncementToUpdate.name = name || AnnouncementToUpdate.name;
+  AnnouncementToUpdate.description =
+    description || AnnouncementToUpdate.description;
+  AnnouncementToUpdate.date = date || AnnouncementToUpdate.date;
+  AnnouncementToUpdate.image = image || AnnouncementToUpdate.image;
+  try {
+    const AnnouncementSaved = await AnnouncementToUpdate.save();
+    res.json(AnnouncementSaved);
+  } catch (error) {
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
 };
 
 export const deleteAnnouncement = async (req, res) => {
- try {
-   const announcement = await Announcement.findById(req.params.id);
-   if(!announcement) {
-     return res.status(404).json({ message: 'Anuncio no encontrado' });
-   }
+  try {
+    const announcement = await Announcement.findById(req.params.id);
+    if (!announcement) {
+      return res.status(404).json({ message: "Anuncio no encontrado" });
+    }
     await Announcement.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'Announcement deleted successfully' });
- } catch (error) {
+    res.status(200).json({ message: "Announcement deleted successfully" });
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
--- index.js
+++ index.js
@@ -13,7 +13,8 @@ import consolidationHousesRoutes from "./routes/ConsolidationHouse.routes.js";
 dotenv.config();
 const app = new express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+
 
 dataBaseConnection();
 
