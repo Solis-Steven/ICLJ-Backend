f6f446a77a36a8eca47cffcb5691ce00d76066ac
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-01-12 17:02:59 -0600

    Cambios controlador e index para multimedia

--- controllers/Multimedia.controller.js
+++ controllers/Multimedia.controller.js
@@ -1,72 +1,76 @@
-import MultimediaContent from '../models/Multimedia.model.js';
+import MultimediaContent from "../models/Multimedia.model.js";
 
 export const getAllMultimediaContents = async (req, res) => {
- try {
-    const multimediaContents = await MultimediaContent.find();
-    res.status(200).json(multimediaContents);
- } catch (error) {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const multimediaContents = await MultimediaContent.find()
+    .select("-createdAt -updatedAt -__v")
+    .skip((page - 1) * limit)
+    .limit(limit);
+    res.json(multimediaContents);
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const createMultimediaContent = async (req, res) => {
- try {
+  try {
     const multimediaContent = new MultimediaContent({
       name: req.body.name,
       ref: req.body.ref,
-      visible: req.body.visible, 
+      visible: req.body.visible,
     });
     await multimediaContent.save();
 
     res.status(201).json(multimediaContent);
- } catch (error) {
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const getMultimediaContent = async (req, res) => {
- try {
+  try {
     const multimediaContent = await MultimediaContent.findById(req.params.id);
-    if(!multimediaContent){
-      return res.status(404).json({ message: 'MultimediaContent not found' });
+    if (!multimediaContent) {
+      return res.status(404).json({ message: "MultimediaContent not found" });
     }
     res.status(200).json(multimediaContent);
- } catch (error) {
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
 
 export const updateMultimediaContent = async (req, res) => {
-   const { id } = req.params;
+  const { id } = req.params;
 
-   const multimediaToUpdate = await MultimediaContent.findById(id);
-   if(!multimediaToUpdate) {
-      const error = new Error("The multimedia Content doesn't exists");
-      return(res.status(404).json({msg: error.message}));
+  const multimediaToUpdate = await MultimediaContent.findById(id);
+  if (!multimediaToUpdate) {
+    const error = new Error("The multimedia Content doesn't exists");
+    return res.status(404).json({ msg: error.message });
   }
-  const {name, ref} = req.body
-    
+  const { name, ref } = req.body;
+
   multimediaToUpdate.name = name || multimediaToUpdate.name;
   multimediaToUpdate.ref = ref || multimediaToUpdate.ref;
-   multimediaToUpdate.visible = req.body.visible || multimediaToUpdate.visible;
-  
- try {
-   const multimediaSave = await multimediaToUpdate.save();
-   res.json(multimediaSave);
- } catch (error) {
-   res.status(500).json({msg: "Internal Server Error"})
- }
+  multimediaToUpdate.visible = req.body.visible || multimediaToUpdate.visible;
+
+  try {
+    const multimediaSave = await multimediaToUpdate.save();
+    res.json(multimediaSave);
+  } catch (error) {
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
 };
 
 export const deleteMultimediaContent = async (req, res) => {
- try {
-   const multimediaContent = await MultimediaContent.findById(req.params.id);
-   if(!multimediaContent){
-      return res.status(404).json({ message: ' MultimediaContent not found' });
-   }
+  try {
+    const multimediaContent = await MultimediaContent.findById(req.params.id);
+    if (!multimediaContent) {
+      return res.status(404).json({ message: " MultimediaContent not found" });
+    }
     await MultimediaContent.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'MultimediaContent deleted successfully' });
- } catch (error) {
+    res.status(200).json({ message: "MultimediaContent deleted successfully" });
+  } catch (error) {
     res.status(400).json({ message: error.message });
- }
+  }
 };
