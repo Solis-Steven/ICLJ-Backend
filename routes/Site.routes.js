4531a2c7dda23e4820a6af15d3b52c0fdcafed9e
Javier <sjavier11x@gmail.com>
2024-01-16 19:46:09 -0600

    delete-auth-gets

--- routes/Site.routes.js
+++ routes/Site.routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.route("/")
     .post(checkAuth, addSite)
-    .get(checkAuth, getAllSites)
+    .get(getAllSites)
 router.route("/:id")
     .put(checkAuth, editSite)
     .delete(checkAuth, deleteSite);
